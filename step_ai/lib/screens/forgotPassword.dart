f086d7fbe7d00f2ebd427ae939e75a3c0c593136
nohiup <nohiup@gmail.com>
2024-10-25 08:00:10 +0700

    Added Login, Signup, PromptGallery Screens

---
+++ step_ai/lib/screens/forgotPassword.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:step_ai/screens/signUp.dart';
+
+class ForgotPasswordApp extends StatelessWidget{
+  const ForgotPasswordApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: ForgotPasswordForm(),
+      )
+    );
+    throw UnimplementedError();
+  }
+}
+
+class ForgotPasswordForm extends StatefulWidget{
+  @override
+  _ForgotPasswordFormState createState() => _ForgotPasswordFormState();
+
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
